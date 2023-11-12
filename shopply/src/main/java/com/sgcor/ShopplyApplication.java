0251b496a510f8cce7ad432e8af766ca9ec1ad5a
Lurldgbodex <professionaleditor6@gmail.com>
2023-11-12 11:54:36 +0100

    project initialization and setup

---
+++ shopply/src/main/java/com/sgcor/ShopplyApplication.java
@@ -0,0 +1,13 @@
+package com.sgcor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopplyApplication.class, args);
+	}
+
+}
---
+++ shopply/src/test/java/com/sgcor/ShopplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgcor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
