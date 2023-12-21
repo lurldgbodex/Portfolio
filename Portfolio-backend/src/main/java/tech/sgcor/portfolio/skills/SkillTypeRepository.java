3dea445d58ced01207e0e55b3b55a1a8c76971ed
Lurldgbodex <professionaleditor6@gmail.com>
2023-12-21 19:26:13 +0100

    implement skill feature of portfolio

---
+++ Portfolio-backend/src/main/java/tech/sgcor/portfolio/skills/SkillTypeRepository.java
@@ -0,0 +1,6 @@
+package tech.sgcor.portfolio.skills;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillTypeRepository extends JpaRepository<SkillType, Long> {
+}
